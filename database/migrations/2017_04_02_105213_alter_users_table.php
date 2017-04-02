a88c60d9799c4ecf1bc093a5ee1a9f61855be3f3
rajkarnik <raj.karnik00@gmail.com>
2017-04-02 17:12:00 +0530

    altered user
    added columns role_id,first_name,last_name,status,contact_no

---
+++ database/migrations/2017_04_02_105213_alter_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->boolean('role_id')->after('id');
+            $table->string('first_name')->after('role_id');
+            $table->string('last_name')->after('first_name');
+            $table->integer('contact_no')->after('last_name');  
+            $table->boolean('status')->after('contact_no');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
