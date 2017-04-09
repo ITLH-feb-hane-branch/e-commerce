0305b0906c84dfd7178c22ef5a36ad31319f4955
prathameshkoshti <prathu48@gmail.com>
2017-04-09 10:08:35 +0530

    Update1(relations)

--- app/OrderDetail.php
+++ app/OrderDetail.php
@@ -8,7 +8,12 @@ class OrderDetail extends Model
 {
     //declaring fillable coloumn names
     protected $fillable = ['action_date' , 'status' ,];
+
     public function shupping(){
       return $this -> belongsTo(ShippingDetail::class);
+    }
+
+    public function user(){
+      return $this -> belongsTo(User::class);
     }
 }
