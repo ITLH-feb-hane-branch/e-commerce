4097d2b7a9495626b3068390931a8ccb23331571
shraddha596 <shraddha596@gmail.com>
2017-04-09 18:00:36 +0530

    Everyone has to perform Migration...!!!!!

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -48,7 +48,9 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => 'required|max:255',
+            'first_name' => 'required|max:255',
+            'last_name' => 'required|max:255',
+            'contact_no' => 'required|min:8|max:10',
             'email' => 'required|email|max:255|unique:users',
             'password' => 'required|min:6|confirmed',
         ]);
@@ -63,7 +65,9 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'name' => $data['name'],
+            'first_name' => $data['first_name'],
+            'last_name' => $data['last_name'],
+            'contact_no' => $data['contact_no'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
         ]);
